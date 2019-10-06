27cd18c3964b13db66d98d1535e240dffa235024
Noah MacFarlane <noahmacf@gmail.com>
2019-10-05 21:25:57 -0400

    added comments

--- run.sh
+++ run.sh
@@ -1,10 +1,14 @@
 #!/bin/bash
 
+# compiles cpp files
 cc -c main.cpp
 cc -c add.cpp
 
+# links the compiled object files
 cc -o add main.o add.o
 
+# runs the executable file
 ./add
 
+# prints the return code
 echo "rc: $?"
